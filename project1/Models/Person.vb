582e76084b30d2d7265f880df32b9e00aab2add4
Dexter <dexterorating@gmail.com>
2026-04-10 18:03:24 +0800

    Implemented the 4 fundamentals of OOP, not sure if that's all or there's something that needs the be addressed...

---
+++ project1/Models/Person.vb
@@ -0,0 +1,9 @@
+﻿Public Class Person
+    ' 1. ENCAPSULATION & INHERITANCE
+    Public Property Id As Integer
+
+    ' 2. POLYMORPHISM
+    Public Overridable Function GetDetails() As String
+        Return "Basic Person Record"
+    End Function
+End Class
